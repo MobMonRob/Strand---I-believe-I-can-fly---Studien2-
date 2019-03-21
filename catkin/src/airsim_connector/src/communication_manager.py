ce06a3f62d0bc585a100abefa129cff7529aeee3
Henri Kohlberg <kohlberg.henri@student.dhbw-karlsruhe.de>
2019-03-21 11:53:19 +0100

    airsim_connector now uses dynamic values for the detected flight instructions instead of binary instructions as it was implemented before. The node also shows the user what to do as long as the calibration process is not finished.

--- catkin/src/airsim_connector/src/communication_manager.py
+++ catkin/src/airsim_connector/src/communication_manager.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import airsim
 import rospy
 from airsim_instruction_builder import AirsimInstructionBuilder
+from instruction import Instruction
+from pose_detection.msg import Calibration as CalibrationMsg
 from pose_detection.msg import Instructions as InstructionsMsg
 
 
@@ -15,7 +18,8 @@ class CommunicationManager:
         self.instruction_builder = AirsimInstructionBuilder()
 
     def start_listening(self):
-        rospy.Subscriber('pose_detection', InstructionsMsg, self.receive_instructions_message)
+        rospy.Subscriber('flight_instructions', InstructionsMsg, self.receive_instructions_message)
+        rospy.Subscriber('calibration_status', CalibrationMsg, self.receive_calibration_status)
 
     def receive_instructions_message(self, instructions_msg):
         self.instruction_builder.reset_instructions()
@@ -24,29 +28,57 @@ class CommunicationManager:
             rospy.logwarn('Unknown command!')
 
         for instruction in instructions_msg.instructions:
-            # TODO: Use instruction builder to build proper commands. Needs probably some refactoring of the drone
-            #  controller.
-            # TODO: Don't use hard coded strings, reuse Pose class from pose_detection node instead.
+
+            # NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
+
             if instruction.instruction == 'HOLD':
                 self.drone.hold()
-            elif instruction.instruction == 'THROTTLE_UP':
-                self.drone.throttle_up()
-            elif instruction.instruction == 'THROTTLE_DOWN':
-                self.drone.throttle_down()
+                return
 
-        """
-        if message.command == 'HOLD':
-            self.drone.hold()
-        elif message.command == 'FORWARD':
-            self.drone.forward()
-        elif message.command == 'TURN_LEFT':
-            self.drone.left()
-        elif message.command == 'TURN_RIGHT':
-            self.drone.right()
-        elif message.command == 'THROTTLE_UP':
-            self.drone.throttle_up()
-        elif message.command == 'THROTTLE_DOWN':
-            self.drone.throttle_down()
-        else:
-            rospy.logwarn('Unknown command!')
-        """
+            if instruction.instruction == 'THROTTLE_UP':
+                scaled_throttle = (((instruction.intensity - 0.0) * (1.0 - 0.5)) / (100.0 - 0.0)) + 0.5
+                self.instruction_builder.add_throttle(scaled_throttle)
+            elif instruction.instruction == 'THROTTLE_DOWN':
+                scaled_throttle = (((instruction.intensity - 0.0) * (0.5 - 0.0)) / (100.0 - 0.0)) + 0.0
+                self.instruction_builder.add_throttle(scaled_throttle)
+
+            if instruction.instruction == 'FORWARD':
+                scaled_pitch = -1.0 * ((((instruction.intensity - 0.0) * (0.7 - 0.1)) / (100.0 - 0.0)) + 0.1)
+                self.instruction_builder.add_pitch(scaled_pitch)
+                self.instruction_builder.add_throttle(0.7)
+
+            if instruction.instruction == 'TURN_LEFT':
+                scaled_yaw = -1 * ((((instruction.intensity - 0.0) * (1.0 - 0.3)) / (100.0 - 0.0)) + 0.3)
+                scaled_roll = -1 * ((((instruction.intensity - 0.0) * (0.6 - 0.0)) / (100.0 - 0.0)) + 0.0)
+                self.instruction_builder.add_yaw(scaled_yaw)
+                self.instruction_builder.add_roll(scaled_roll)
+                self.instruction_builder.add_throttle(0.6)
+            elif instruction.instruction == 'TURN_RIGHT':
+                scaled_yaw = (((instruction.intensity - 0.0) * (1.0 - 0.3)) / (100.0 - 0.0)) + 0.3
+                scaled_roll = (((instruction.intensity - 0.0) * (0.6 - 0.0)) / (100.0 - 0.0)) + 0.0
+                self.instruction_builder.add_yaw(scaled_yaw)
+                self.instruction_builder.add_roll(scaled_roll)
+                self.instruction_builder.add_throttle(0.6)
+
+        self.drone.by_instruction(self.instruction_builder.get_instructions())
+
+    def receive_calibration_status(self, calibration_msg):
+        if calibration_msg.status == 'STARTED_2D':
+            self.drone.print_message('2D-Calibration process started!')
+            self.drone.print_message('Please stand in the middle of the camera image and hold the default ' +
+                                     'position for 3 seconds. Stretch both arms to the side with a 90 degree angle.')
+        elif calibration_msg.status == '2D_RESET_TIMER':
+            self.drone.print_message('2D-Calibration restarted! Please hold the default position for 3 seconds. ' +
+                                     'Stretch both arms to the side with a 90 degree angle.', 2)
+        elif calibration_msg.status == '2D_MISSING_POINTS':
+            self.drone.print_message('Error during 2D-Calibration: Not all important points can be seen by the ' +
+                                     'camera! Please make sure that your arms, shoulders and hip are visible for the ' +
+                                     'camera.', 2)
+        elif calibration_msg.status == '2D_ARMS_NOT_ON_SHOULDER_HEIGHT':
+            self.drone.print_message('Error during 2D-Calibration: Your arms are not on the same height as your ' +
+                                     'shoulders! Please stretch both arms to the side with a 90 degree angle!', 2)
+        elif calibration_msg.status == '2D_ARMS_NOT_STRETCHED':
+            self.drone.print_message('Error during 2D-Calibration: Your arms are not fully stretched out! ' +
+                                     'Please stretch both arms to the side with a 90 degree angle!', 2)
+        elif calibration_msg.status == 'FINISHED_2D':
+            self.drone.print_message('2D-Calibration finished successfully!', 1)
--- catkin/src/airsim_connector/src/drone_controller.py
+++ catkin/src/airsim_connector/src/drone_controller.py
@@ -3,6 +3,7 @@
 import airsim
 import time
 import rospy
+from instruction import Instruction
 
 
 class DroneController:
@@ -27,27 +28,20 @@ class DroneController:
         self.client.enableApiControl(False)
         self.flying = False
 
-    def forward(self):
-        self.set_next_action(self.client.moveByAngleThrottleAsync, -0.3, 0, 0.7, 0, self.update_rate)
-
     def hold(self):
-        self.set_next_action(self.client.hoverAsync)
+        if self.flying:
+            self.set_next_action(self.client.hoverAsync)
 
-    def left(self):
-        self.set_next_action(self.client.moveByAngleThrottleAsync, -0.1, -0.3, 0.75, -0.3, self.update_rate)
-
-    def right(self):
-        self.set_next_action(self.client.moveByAngleThrottleAsync, -0.1, 0.3, 0.75, 0.3, self.update_rate)
-
-    def throttle_up(self):
-        self.reset_next_action()
-        if not self.flying:
+    def by_instruction(self, instruction):
+        if not self.flying and instruction[Instruction.THROTTLE] > 0:
             self.takeoff()
-        else:
-            self.set_next_action(self.client.moveToZAsync, self.client.getPosition().z_val - 100, 5, self.update_rate)
-
-    def throttle_down(self):
-        self.set_next_action(self.client.moveToZAsync, self.client.getPosition().z_val + 200, 5, self.update_rate)
+        elif self.flying:
+            self.set_next_action(self.client.moveByAngleThrottleAsync,
+                                 instruction[Instruction.PITCH],
+                                 instruction[Instruction.ROLL],
+                                 instruction[Instruction.THROTTLE],
+                                 instruction[Instruction.YAW],
+                                 self.update_rate)
 
     def set_next_action(self, function, *args):
         self.next_action = [function, args]
@@ -67,3 +61,6 @@ class DroneController:
         else:
             rospy.logwarn('No upcoming action specified! Waiting for new user input.')
         time.sleep(self.update_rate)
+
+    def print_message(self, message, severity = 0):
+        self.client.simPrintLogMessage(message, severity = severity)
