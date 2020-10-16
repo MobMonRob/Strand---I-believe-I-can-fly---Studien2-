adc39594a77b3f6f51c5c14f00fddf8fa7b6e487
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-10-16 13:07:46 +0200

    safer hard_clean

--- hard_clean.sh
+++ hard_clean.sh
@@ -2,11 +2,13 @@
 
 cd "$(dirname "$BASH_SOURCE")"
 
+read -rsp "sure?" -n 1 pause1
+
 #remove untracked files and folders
 git clean -d -ff -x &> /dev/null
 
 #remove ignored tracked files and folders
 git ls-files -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
 
-echo "cleared hard"
+echo "cleaned hard"
 
